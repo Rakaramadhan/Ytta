e1bb2673934b07cc7d0cfddc1e351b4c3c9a0180
Rakaramadhan <103983741+Rakaramadhan@users.noreply.github.com>
2022-12-23 12:43:23 +0700

    Update hwkal.js
    .

--- hwkal.js
+++ hwkal.js
@@ -20,20 +20,20 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/c/HwModsWa857' // ubah aia
 global.ig = 'https://www.instagram.com/hikal_857/?hl=id' // ubah aja
-global.email = 'denyp857@gmail.com'
-global.region = 'indonesia'
+global.email = 'KhaTheHard@gmail.com'
+global.region = 'isekai'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'KhaTheInvisble' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Aisyah-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'KhaTheDarkness' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281214281312'] // ubah aja pake nomor lu
-global.premium = ['6281214281312'] // ubah aja pake nomor lu
-global.packname = 'HW MODS WA' // ubah aja
-global.ownerr = ['HW MODS WA'] // ubaha aja
-global.author = 'HW MODS WA' //ubah aja
-global.sessionName = './sessionye/haikal' //Gausaj Di Ubah
+global.owner = ['62882006212045'] // ubah aja pake nomor lu
+global.premium = ['62882006212045'] // ubah aja pake nomor lu
+global.packname = 'ItsMeKhaa' // ubah aja
+global.ownerr = ['KhaTheInvisble'] // ubaha aja
+global.author = 'ItsMeRaka?' //ubah aja
+global.sessionName = './sessionye/session' //Gausaj Di Ubah
 global.prefa = ['','!','.',',','🐤','🗿'] // Gausah Mendingan 
 global.sp = '⭔' // Gausah Juga
 
